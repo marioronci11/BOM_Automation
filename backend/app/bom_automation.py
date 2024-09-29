9d86c2b92e10826b32bbfacd6a4e80ccc0bb57b8
Mario Ronci <roncidesigns11@gmail.com>
2024-09-29 00:25:28 -0400

    Created a FastAPI for the app to connect with the web at localhost

--- backend/app/bom_automation.py
+++ backend/app/bom_automation.py
@@ -1,5 +1,11 @@
 #The script that will modify the excel sheet and part number
+# from flask import Flask
+# app = Flask(__name__)
 
-from flask import Flask
+from fastapi import FastAPI
 
+app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "BOM Automation Tool is Running!"}
